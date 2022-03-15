e177ff18f822e0795ed83cc9712c1c95d4b3aae9
hsh0340 <hsh0340@naver.com>
2022-03-15 23:41:06 +0900

    commit

---
+++ workspace_js/js0314/js/basic01.js
@@ -0,0 +1,9 @@
+// 2. 외부 자바스크립트 작성
+// 자바스크립트의 효율적인 관리와 작성을 위해서 사용
+let t1 = "3월도 벌써 중반입니다.";
+let t2 = "공부에는 노력외에 왕도는 없습니다.";
+function welcome2() {
+  document.write(t1 + "<br>");
+  document.write(t2);
+}
+welcome2();
